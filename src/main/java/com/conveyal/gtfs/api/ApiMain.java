599ad4516e4af53311e4f466e55f642ee782cd58
Landon Reed <landontreed@gmail.com>
2016-02-09 19:39:23 -0500

    load gtfs feeds from s3

--- src/main/java/com/conveyal/gtfs/api/ApiMain.java
+++ src/main/java/com/conveyal/gtfs/api/ApiMain.java
@@ -1,79 +1,123 @@
 package com.conveyal.gtfs.api;
 
-import com.conveyal.gtfs.api.*;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.conveyal.gtfs.GTFSFeed;
 import com.conveyal.gtfs.api.models.FeedSource;
-import com.conveyal.gtfs.model.Agency;
-import com.conveyal.gtfs.model.*;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Maps;
-import com.googlecode.concurrenttrees.common.PrettyPrinter;
-import com.googlecode.concurrenttrees.radix.ConcurrentRadixTree;
-import com.googlecode.concurrenttrees.radix.RadixTree;
-import com.googlecode.concurrenttrees.radix.node.concrete.DefaultCharArrayNodeFactory;
-import com.googlecode.concurrenttrees.radix.node.util.PrettyPrintable;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.index.strtree.STRtree;
+import org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
-
-import static spark.Spark.*;
+import java.util.zip.ZipInputStream;
 
 /**
  * Created by landon on 2/3/16.
  */
 public class ApiMain {
-    public static GTFSFeed feed;
-    public static ArrayList<GTFSFeed> feeds;
-    public static Map feedAgencies;
-    public static Map<String, GTFSFeed> feedMap;
-//    public static STRtree routeIndex;
-//    public static STRtree stopIndex;
-//    public static RadixTree<Stop> stopTree;
-//    public static RadixTree<Route> routeTree;
     public static Map<String, FeedSource> feedSources;
+    public static final Properties config = new Properties();
+    private static String s3credentials;
+    private static AmazonS3Client s3;
+    private static String feedBucket;
+    private static ObjectListing gtfsList;
+    private static boolean workOffline;
+    public static void main(String[] args) throws Exception {
+        FileInputStream in;
 
-    public static void main(String[] args){
+        if (args.length == 0)
+            in = new FileInputStream(new File("application.conf"));
+        else
+            in = new FileInputStream(new File(args[0]));
+
+        config.load(in);
 
         initialize();
-
         Routes.routes();
     }
 
-    /** initialize the database */
-    public static void initialize () {
-//        Load GTFS datasets
-//        GTFSFeed[] feeds = new GTFSFeed[];
-//        ApiMain.feeds = new ArrayList<GTFSFeed>();
-//        ApiMain.feedAgencies = Maps.newHashMap();
-//        ApiMain.feedMap = Maps.newHashMap();
-        ApiMain.feedSources = Maps.newHashMap();
-//        ApiMain.feed = GTFSFeed.fromFile("/Users/landon/Downloads/google_transit.zip");
-        String[] listOfFeeds = new String[] {"/Users/landon/Downloads/google_transit.zip", "/Users/landon/Downloads/google_transit_staten_island.zip"};
-        int count = 0;
-        for (String feedPath : listOfFeeds){
-            String feedId = "feed-" + String.valueOf(count);
-            System.out.println("Loading feed: " + feedId);
-            System.out.println("Loading feed: " + feedPath);
-//            GTFSFeed feed = GTFSFeed.fromFile(feedPath);
-//            FeedSource feedSource = new FeedSource(feedPath);
-            ApiMain.feedSources.put(feedId, new FeedSource(feedPath));
-            System.out.println(feedSources.get(feedId).feed.routes.values().iterator().next().route_short_name);
-//            ApiMain.feeds.add(feed);
-//            ApiMain.feedAgencies.put(feedId, feed.agency);
-//            ApiMain.feedMap.put(feedId, feed);
-            count++;
+    /**
+     * initialize the database
+     */
+    public static void initialize() throws IOException {
+        // Connect to s3
+        workOffline = Boolean.parseBoolean(
+                ApiMain.config.getProperty("s3.work-offline"));
+        s3credentials = ApiMain.config.getProperty("s3.aws-credentials");
+        feedBucket = ApiMain.config.getProperty("s3.feeds-bucket");
+
+        if(!workOffline) {
+            if (s3credentials != null) {
+                AWSCredentials creds = new ProfileCredentialsProvider(s3credentials, "default").getCredentials();
+                s3 = new AmazonS3Client(creds);
+            }
+            else {
+                // default credentials providers, e.g. IAM role
+                s3 = new AmazonS3Client();
+            }
+
+
+            gtfsList = s3.listObjects(feedBucket);
+//            s3.getbucket;
         }
 
+
+        // Load GTFS datasets
+
+        // Use s3
+        ApiMain.feedSources = Maps.newHashMap();
+
+        if(!workOffline) {
+            int count = 0;
+            for (S3ObjectSummary objSummary : gtfsList.getObjectSummaries()){
+                String feedId = objSummary.getKey();
+                System.out.println("Loading feed: " + feedId);
+                InputStream obj = s3.getObject(feedBucket, objSummary.getKey()).getObjectContent();
+
+                // create tempfile so we can pass GTFSFeed.fromFile a string file path
+                File tempFile = File.createTempFile("test", ".zip");
+                tempFile.getAbsolutePath();
+                tempFile.deleteOnExit();
+                try (FileOutputStream out = new FileOutputStream(tempFile)) {
+                    IOUtils.copy(obj, out);
+                }
+                ApiMain.feedSources.put(feedId, new FeedSource(tempFile.getAbsolutePath()));
+
+                count++;
+                // break after one feed is loaded from aws.
+                if (count > 0){
+                    break;
+                }
+            }
+        }
+
+        // Use hard-coded paths
+        else{
+            String[] listOfFeeds = new String[]{"/Users/landon/Downloads/google_transit.zip", "/Users/landon/Downloads/google_transit_staten_island.zip"};
+            int count = 0;
+            for (String feedPath : listOfFeeds) {
+                String feedId = "feed-" + String.valueOf(count);
+                System.out.println("Loading feed: " + feedId);
+                System.out.println("Loading feed: " + feedPath);
+                ApiMain.feedSources.put(feedId, new FeedSource(feedPath));
+                count++;
+            }
+        }
+
+
     }
+
     public GTFSFeed getFeedByAgency(String agencyId) {
         return new GTFSFeed();
     }
 
-    public String[] getAgencyIdsForFeed(GTFSFeed feed){
+    public String[] getAgencyIdsForFeed(GTFSFeed feed) {
 //        String[] agencies = new String[]{};
+
         return feed.agency.keySet().toArray(new String[feed.agency.size()]);
 //        for(Agency agency : feed.agency.){
 //            if (key == "agency_id"){
@@ -81,24 +125,6 @@ public class ApiMain {
 //            }
 //        }
 //        return agencies;
-    }
-
-    public Map<List<String>, List<String>> getStopsNearby(String lat, String lon){
-
-
-
-        Map<List<String>, List<String>> nearbyStops = Maps.newHashMap();
-
-        int radius = 1; // default radius is 1 mile
-        nearbyStops = getStopsNearby(lat, lon, radius);
-
-        return nearbyStops;
-    }
-
-    public Map<List<String>, List<String>> getStopsNearby(String lat, String lon, int radius){
-        Map<List<String>, List<String>> nearbyStops = Maps.newHashMap();
-
-        return nearbyStops;
     }
 
 }
