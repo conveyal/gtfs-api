e4fbc1e3d34ce4fab8c836db5b4641c72717dfe2
Landon Reed <landontreed@gmail.com>
2017-05-12 12:47:30 -0400

    style(Routes): remove old commented out code

--- src/main/java/com/conveyal/gtfs/api/Routes.java
+++ src/main/java/com/conveyal/gtfs/api/Routes.java
@@ -13,8 +13,6 @@ import com.conveyal.gtfs.api.controllers.JsonTransformer;
  */
 public class Routes {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
-
     private static final JsonTransformer json = new JsonTransformer();
     public static void routes() {
         routes("");
@@ -29,17 +27,6 @@ public class Routes {
         get(prefix + "/hello", (request, response) -> "Hello, you!");
         get(prefix + "/feeds", FeedController::getFeeds, json);
         get(prefix + "/feeds/:id", FeedController::getFeeds, json);
-//        get(prefix + "/feeds/:feed_id/agencies", FeedController::getFeeds, json);
-
-//        get(prefix + "/feeds/:feed_id/agencies/:id", (request, response) -> {
-//            System.out.println(request.params());
-//            System.out.println(request.queryParams());
-//            System.out.println("agency request");
-//            return mapper.writeValueAsString(
-//                    ApiMain.feedMap.get(request.params("feed_id"))
-//                            .agency.get(request.params("id"))
-//            );
-//        });
 
         // Routes
         get(prefix + "/routes", RoutesController::getRoutes, json);
@@ -50,19 +37,9 @@ public class Routes {
         get(prefix + "/stops", StopsController::getStops, json);
         get(prefix + "/stops/:id", StopsController::getStops, json);
 
-        // TODO: Add endpoint for name search for both routes and stops.
-
-//        get(prefix + "/patterns", (request, response) -> mapper.writeValueAsString(ApiMain.feed.findPatterns()));
-//        get(prefix + "/patterns/:id", (request, response) -> mapper.writeValueAsString(ApiMain.feed.findPatterns()));
-//        get(prefix + "/shapes", (request, response) -> mapper.writeValueAsString(ApiMain.feed.shapes));
-//        get(prefix + "/shapes/:id/shapepoints", (request, response) -> mapper.writeValueAsString(ApiMain.feed.shapes.get(request.params("id"))));
-
 //        TODO:'/trips' route is causing infinite recursion between com.conveyal.gtfs.model.Service["calendar"]->com.conveyal.gtfs.model.Calendar["service"]
-//        get(prefix + "/trips", (request, response) -> mapper.writeValueAsString(ApiMain.feed.trips));
         get(prefix + "/trips", TripsController::getTrips, json);
-        get(prefix + "/trips/:id", TripsController::getTrips, json);
-//                mapper.writeValueAsString(ApiMain.getFeedSource().trips.get(request.params("id")))
-//        );
+
         get(prefix + "/trips/:id/stoptimes", StopTimesController::getStopTimes, json);
 
         get(prefix + "/patterns", PatternsController::getPatterns, json);
