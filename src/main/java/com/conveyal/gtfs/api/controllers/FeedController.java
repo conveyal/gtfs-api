8b471f0a1ed7c2b573774931175cc47674694ee9
Landon Reed <landontreed@gmail.com>
2017-05-30 11:53:59 -0400

    feat(FeedController): add option to specify list of feedIDs to add to cache

--- src/main/java/com/conveyal/gtfs/api/controllers/FeedController.java
+++ src/main/java/com/conveyal/gtfs/api/controllers/FeedController.java
@@ -1,19 +1,35 @@
 package com.conveyal.gtfs.api.controllers;
 
 import com.conveyal.gtfs.api.ApiMain;
+import com.conveyal.gtfs.model.Agency;
 import spark.Request;
 import spark.Response;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by landon on 2/9/16.
  */
 public class FeedController {
-    public static Object getFeeds(Request req, Response res){
+    public static List<Object> getFeeds(Request req, Response res){
+        // return all currently registered feeds if no param specified
         if (req.params().size() == 0 && req.queryParams().size() == 0){
-            return ApiMain.registeredFeedSources;
+            return ApiMain.registeredFeedSources.stream()
+                    .collect(Collectors.toList());
         }
+        // get and return specific feed for single param
         if (req.params("id") != null){
-            return ApiMain.getFeedSource(req.params("id")).feed.agency.values();
+            return ApiMain.getFeedSource(req.params("id")).feed.agency.values().stream()
+                    .collect(Collectors.toList());
+        }
+        // get and return multiple specified feeds for comma separated list param
+        if (req.queryParams("id") != null){
+            List<String> idList = Arrays.asList(req.queryParams("id").split(","));
+            ApiMain.getFeedSources(idList);
+            return ApiMain.registeredFeedSources.stream()
+                    .collect(Collectors.toList());
         }
         return null;
     }
