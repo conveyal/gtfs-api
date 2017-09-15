49fec01bc85281b7b95f37d8f154d03305fa39c4
Andrew Byrd <andrew@fastmail.net>
2017-09-15 23:15:42 +0200

    comments

--- src/main/java/com/conveyal/gtfs/api/GraphQLMain.java
+++ src/main/java/com/conveyal/gtfs/api/GraphQLMain.java
@@ -17,14 +17,13 @@ import static spark.Spark.*;
  *
  * What we're trying to provide is this:
  * The queries that analysis-ui makes are at https://github.com/conveyal/analysis-ui/blob/dev/lib/graphql/query.js ; note that feeds are wrapped in bundles in analysis-ui (we wrap the GTFS API types)
-
- GraphQL queries for datatools-ui are at https://github.com/catalogueglobal/datatools-ui/blob/dev/lib/gtfs/util/graphql.js.
-
- We also use a few of the REST endpoints in datatools-ui, including:
- stops/routes by bounding box
- stop/routes by text string search (route_long_name/route_short_name, stop_name/stop_id/stop_code)
- Feeds - to get a list of the feed_ids that have been loaded into the gtfs-api
-
+ * GraphQL queries for datatools-ui are at https://github.com/catalogueglobal/datatools-ui/blob/dev/lib/gtfs/util/graphql.js.
+ *
+ * We will eventually want to replace some of the REST-ish endpoints in datatools-ui, including:
+ * stops/routes by bounding box
+ * stop/routes by text string search (route_long_name/route_short_name, stop_name/stop_id/stop_code)
+ * Feeds - to get a list of the feed_ids that have been loaded into the gtfs-api
+ *
  * Here are some sample database URLs
  * H2_FILE_URL = "jdbc:h2:file:~/test-db"; // H2 memory does not seem faster than file
  * SQLITE_FILE_URL = "jdbc:sqlite:/Users/abyrd/test-db";
