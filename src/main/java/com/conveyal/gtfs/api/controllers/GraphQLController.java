2672234b923bece0216406c246c6b3b7d8b3a34f
aguiraf <aguiraf@gmail.com>
2017-11-08 17:26:45 +0100

    Wrap schema response in "data" object

--- src/main/java/com/conveyal/gtfs/api/controllers/GraphQLController.java
+++ src/main/java/com/conveyal/gtfs/api/controllers/GraphQLController.java
@@ -57,15 +57,17 @@ public class GraphQLController {
             halt(400, "Malformed JSON");
         }
         // FIXME converting String to JSON nodes and back to string, then re-parsing to Map.
-        String vars = node.get("variables").asText();
+        String vars = (node.get("variables") != null) ? node.get("variables").asText() : "null";
         String query = node.get("query").asText();
         return doQuery(vars, query, response);
     }
 
     private static Object doQuery (String varsJson, String queryJson, Response response) {
         long startTime = System.currentTimeMillis();
-        if (varsJson == null && queryJson == null) {
-            return GRAPHQL.execute(IntrospectionQuery.INTROSPECTION_QUERY).getData();
+        if ((varsJson == null && queryJson == null)
+                //a slightly different IntrospectionQuery is sent by GraphiQL itself
+            || (queryJson.indexOf("IntrospectionQuery")>0)) {
+            return GRAPHQL.execute(IntrospectionQuery.INTROSPECTION_QUERY);
         }
         try {
             Map<String, Object> variables = mapper.readValue(varsJson, new TypeReference<Map<String, Object>>(){});
